2920b961147d05aed5a83e2e32f5f5cb113c2fb7
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-16 18:08:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Write your solution in this file!
 var customerName
-function upperCaseCustomerName(customerName)
-customerName.toUpperCase()
+function upperCaseCustomerName(customerName){
+  customerName.toUpperCase()
+}
+
