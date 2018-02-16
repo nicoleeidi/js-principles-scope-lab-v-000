36c5cda9874a8c8c79e2e24ead8404fa3eb38533
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-16 18:11:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ function overwriteBestCustomer(){
 }
 
 
-var leastFavoriteCustomer = 'tom'
+const leastFavoriteCustomer = 'tom'
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'rob'
+}
